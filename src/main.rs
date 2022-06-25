a915b3399c8dbc7d44fb3c745e22235ea55f4902
Crinax <agreedeth@gmail.com>
2022-06-25 03:29:03 +0300

    Complete guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     // Print something
@@ -6,15 +8,33 @@ fn main() {
 
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    // create mutable variable and call String constructor
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    // await input form user and put it to variable
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // create mutable variable and call String constructor
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        // await input form user and put it to variable
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
